1260fc96220c68e33c761c4cd04e13d96e1be5fd
salino3 <flaviobcn012@gmail.com>
2023-04-05 21:04:11 +0200

    cambios en config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 import sass from "sass";
 
 export default defineConfig({
-  base: './',
+  base: "./",
+  assetsInclude: ["**/*.jpg"],
+
   plugins: [react()],
   css: {
     preprocessorOptions: {
