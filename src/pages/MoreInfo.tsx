1d6e347e683fd92212701a184c921f3c7f64db3d
salino3 <flaviobcn012@gmail.com>
2023-04-11 08:53:06 +0200

    cambios 2

--- src/pages/MoreInfo.tsx
+++ src/pages/MoreInfo.tsx
@@ -1,48 +1,48 @@
-import React from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import { GlobalData, MyState, ProductInfo } from '../core';
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { GlobalData, MyState, ProductInfo } from "../core";
 
 export const MoreInfo: React.FC = () => {
-  
-  const {state, CheckboxChange} = React.useContext<MyState>(GlobalData);
-  
+  const { state, CheckboxChange } = React.useContext<MyState>(GlobalData);
+
   const navigate = useNavigate();
-  const {id} = useParams();
+  const { id } = useParams();
 
+  const product: ProductInfo | undefined = state?.products?.find(
+    (item: ProductInfo) => item.id === Number(id)
+  );
 
-  const product: ProductInfo | undefined = state?.products?.find((item: ProductInfo) => item.id === Number(id));
-
- 
-if (product) {
-  return (
-    <div className="m-auto ">
-      <h5
-        className="navigateBack text-center text-info m-1 mb-3"
-        onClick={() => navigate(-1)}
-      >
-        <u>Go back</u>
-      </h5> 
-      <div className="card2 ">
-        <div className="m-1">{product?.id}</div>
-        <img src={`../../public/assets/${product?.picUrl}`} alt="product" />
-        <div className="texts">
-          <h5 className="text-success m-2">
-            title: <span>{product?.title}</span>
-          </h5>
-          <h6 className="myCheckBox text-success m-2">
-            Price: <span>{product?.price} €</span>
-            <input
-              type={"checkbox"}
-              onChange={() => CheckboxChange(product?.id)}
-              checked={product?.selected}
-            />
-          </h6>
+  if (product) {
+    return (
+      <div className="m-auto ">
+        <h5
+          className="navigateBack text-center text-info m-1 mb-3"
+          onClick={() => navigate(-1)}
+        >
+          <u>Go back</u>
+        </h5>
+        <div className="card2 ">
+          <div className="m-1">{product?.id}</div>
+          <img src={`/info/${product?.picUrl}`} alt="product" />
+          <div className="texts">
+            <h5 className="text-success m-2">
+              title: <span>{product?.title}</span>
+            </h5>
+            <h6 className="myCheckBox text-success m-2">
+              Price: <span>{product?.price} €</span>
+              <input
+                type={"checkbox"}
+                onChange={() => CheckboxChange(product?.id)}
+                checked={product?.selected}
+              />
+            </h6>
+          </div>
         </div>
       </div>
-    </div>
-  );
-    } else {
-  return <h1 className="text-center m-2  text-warning">Product Not Found...</h1>;
- }
-  
-}
+    );
+  } else {
+    return (
+      <h1 className="text-center m-2  text-warning">Product Not Found...</h1>
+    );
+  }
+};
